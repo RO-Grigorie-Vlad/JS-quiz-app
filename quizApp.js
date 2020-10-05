22dbf96037ce1d8f109a3292dbb8e621732127c2
Grigorie Vlad Ioan <vlad_grigorie@yahoo.com>
2020-10-05 13:24:12 +0300

    Finished authentification system

---
+++ quizApp.js
@@ -0,0 +1,267 @@
+var AccountsController = (function(){
+
+    var Account = function(id, firstName, lastName, email, username, password){
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+    
+    //mock database + loggedIn Accounts (to keep track of logged-in users)
+    var data = {
+        accounts: [],
+        loggedInAccounts: [] // in this JS-only-no-backEnd-App there will be a maximum of one session;
+    };
+
+    var download = function (content, fileName, contentType) {
+        var a = document.createElement("a");
+        var file = new Blob([content], {type: contentType});
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+    }
+
+    var convertToJSON =  function(data){
+        return JSON.stringify(data);
+    };
+
+    var downloadJson =  function(jsonData){
+        download(jsonData, 'accounts.json', 'json');
+    };
+
+    return {
+        getData: function(){
+            return data;
+        },
+        register: function(formData){
+            var idForNewAccount;
+            var noDuplicateEmail = true;
+        
+            if(data.accounts.length > 0){
+                idForNewAccount = data.accounts[data.accounts.length -1].id + 1;
+            }
+            else{
+                idForNewAccount = 0;
+            }
+            data.accounts.forEach(account => {
+                if(formData[2] === account.email){
+                    noDuplicateEmail = false;
+                }
+            });
+
+            if(noDuplicateEmail === true){
+                var newAccount = new Account(idForNewAccount, formData[0], formData[1], formData[2], formData[3], formData[4]);
+                console.log(newAccount);
+                data.accounts.push(newAccount);
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+            
+        },
+        login: function(formData){
+            var correctCredentials = false;
+            var username, id;
+            data.accounts.forEach(account => {
+                if(account.email === formData[0]){
+                    if(account.password === formData[1]){
+                        // Correct login credentials
+                        correctCredentials = true;
+                        username = account.username;
+                        id = account.id;
+                        // add account to the currently logged-in users list
+                        if(!data.loggedInAccounts.includes(account.id)){
+                            data.loggedInAccounts.push(account.id);
+                        }
+                    }
+                }
+            });
+            if(correctCredentials === false){
+                return false;
+            }
+            else{
+                return [true, username, id];
+            }
+        },
+        logout: function(id){
+            // remove logged-in user from the data.loggedInAccounts list
+
+            var index = data.loggedInAccounts.indexOf(id);
+            if (index > -1) {
+                data.loggedInAccounts.splice(index, 1);
+            }
+
+            //data.loggedInAccounts = [];
+            
+        },
+        getAccountsJSONfile(){
+            downloadJson(convertToJSON(data.accounts))
+        }     
+    };
+})();
+
+var QuizController = (function(){
+
+})();
+
+var UIController = (function(){
+
+    var DOMstrings = {
+        registerForm: '#registerForm',
+        loginForm: '#loginForm',
+        registerButton: '#registerButton',
+        loginButton: '#loginButton',
+        submitRegister: '#submitRegister',
+        submitLogin: '#submitLogin',
+        hiddenElem: 'hiddenElem',
+        visibility: 'visibility',
+        message: '#message',
+        loginStatus: '#loginStatus',
+        logout: '#logout',
+        logoutButton: '#logoutButton'
+    };
+
+    
+    var logoutH4 = document.querySelector(DOMstrings.logout);
+    var loginStatus = document.querySelector(DOMstrings.loginStatus);
+
+    return {
+        getDOMstrings: function(){
+            return DOMstrings;
+        },
+        //get the data introduced by the user in the Register Form
+        getRegisterFormData: function(){
+            var registerForm = document.querySelector(DOMstrings.registerForm);
+            var formData = [];
+            for(var i = 0; i < registerForm.elements.length - 1; i++){ // all elements, minus the submit button
+                formData.push(registerForm.elements[i].value);
+            }
+            registerForm.reset(); // clear the form
+            return formData;
+        },
+        //get the data introduced by the user in the Login Form
+        getLoginFormData: function(){
+            var loginForm = document.querySelector(DOMstrings.loginForm);
+            var formData = [];
+            for(var i = 0; i < loginForm.elements.length - 1; i++){ // all elements, minus the submit button
+                formData.push(loginForm.elements[i].value);
+            }
+            loginForm.reset(); // clear the form
+            return formData;
+        },
+        // display a message in a paragraph (succes/fail on register and login)
+        displayMessage: function(msg){
+            var messageParagraph = document.querySelector(DOMstrings.message);
+            messageParagraph.innerHTML = msg;
+            
+            setTimeout(function(){
+                messageParagraph.innerHTML = '';
+            }, 2000);
+        },
+        // type = 1 for login, and 2 for login
+        updateLoginStatus: function(msg){
+            console.log('updateLoginStatus() called');
+            //logoutH4.classList.toggle(DOMstrings.hiddenElem);
+            //loginStatus.classList.toggle(DOMstrings.hiddenElem);
+            //var logout = document.querySelector(DOMstrings.logout);
+            //logout.classList.toggle(DOM.hiddenElem);
+            logoutH4.innerHTML = msg + "You can <a id='logoutButton'>Logout</a>";
+            
+        }
+    };
+})();
+
+var mainController = (function(QuizCtrl, AccCtrl, UICtrl){
+
+    var DOM = UICtrl.getDOMstrings();
+
+    var registerForm = document.querySelector(DOM.registerForm);
+    var loginForm = document.querySelector(DOM.loginForm);
+
+    var submitRegister = document.querySelector(DOM.submitRegister);
+    var submitLogin = document.querySelector(DOM.submitLogin);
+
+    var setupEventListeners = function(){
+        //toggle witch forms are displayed
+        document.querySelector(DOM.registerButton).addEventListener('click', function(){
+            if(!loginForm.classList.contains(DOM.hiddenElem)){
+                loginForm.classList.toggle(DOM.hiddenElem);
+            }
+            registerForm.classList.toggle(DOM.hiddenElem);
+        });
+        document.querySelector(DOM.loginButton).addEventListener('click', function(){
+            if(!registerForm.classList.contains(DOM.hiddenElem)){
+                registerForm.classList.toggle(DOM.hiddenElem);
+            }
+            loginForm.classList.toggle(DOM.hiddenElem);
+        });
+
+        submitRegister.addEventListener("click", registerNewAccount);
+        submitLogin.addEventListener("click", login);
+        
+        document.querySelector(DOM.logoutButton).addEventListener("click", logout);
+    };
+    
+    var logoutH4 = document.querySelector(DOM.logout);
+    var loginStatus = document.querySelector(DOM.loginStatus);
+
+    var loggedInAccountID;
+
+    // Login
+    var login = function(){
+        var formData = UICtrl.getLoginFormData();
+        console.log(formData);
+        var loggedInSuccesfully = AccCtrl.login(formData);
+        if(loggedInSuccesfully === false){
+            UICtrl.displayMessage('Wrong credentials, try again!'); 
+        }
+        else{
+            UICtrl.displayMessage('Successful login!');
+            var msg = 'Logged in as ' + loggedInSuccesfully[1] + '. ';
+            UICtrl.updateLoginStatus(msg);
+
+            logoutH4.classList.toggle(DOM.hiddenElem);
+            loginStatus.classList.toggle(DOM.hiddenElem);
+
+            //when the element dissapears, the eventListener dissapears as well
+            //therefor we have to reasign it - wierd
+            document.querySelector(DOM.logoutButton).addEventListener("click", logout);
+            loggedInAccountID = loggedInSuccesfully[2];
+
+        }
+    };
+
+    //Logout
+    var logout = function(){
+        AccCtrl.logout(loggedInAccountID);
+        loggedInAccountID = null;
+        logoutH4.classList.toggle(DOM.hiddenElem);
+        loginStatus.classList.toggle(DOM.hiddenElem);
+    };
+
+    // Register a new Account
+    var registerNewAccount = function(){
+        var formData = UICtrl.getRegisterFormData();
+        console.log(formData);
+        var registerSuccesfull = AccCtrl.register(formData);
+        if(registerSuccesfull){
+            UICtrl.displayMessage('New account created successfully!');
+        }else{
+            UICtrl.displayMessage('There already is an account with this email!');
+        }
+    };
+
+    return {
+        init: function(){
+            console.log('App Started!');
+            setupEventListeners();
+        }
+    }
+})(QuizController, AccountsController, UIController);
+
+mainController.init();
+
